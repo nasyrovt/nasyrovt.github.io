1ddcc07d2d90376eeae1eee516b98030207fb029
Nasyrov Takhir <64357944+nasyrovt@users.noreply.github.com>
2022-05-02 21:32:11 +0200

    Images

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -11,13 +11,13 @@ const Header = () => {
             <div className="container header__container">
                 <h5>Hello I'm</h5>
                 <h1>NASYROV Takhir</h1>
-                <h5 className="text-light">Game and Web Developer</h5>
+                <h5 className="text-light">Passionate Game Developer</h5>
                 <CTA />
                 <HeaderSocials />
                 <div className="me">
                     <img src={ME} alt="me" />
                 </div>
-                <a href="#contact" className='scroll__down'>Scroll Down</a>
+                <a href="#contact" className='scroll__down'>Contact Me</a>
             </div>
         </header>
     )
